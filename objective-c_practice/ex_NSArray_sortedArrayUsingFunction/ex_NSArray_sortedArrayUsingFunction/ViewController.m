f382231ec8e70e0f6756abc220e9df5a12d44c60
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-03 16:38:00 +0900

    [chore] 自作関数による配列のソート例(p.84-85)

--- objective-c_practice/ex_NSArray_sortedArrayUsingFunction/ex_NSArray_sortedArrayUsingFunction/ViewController.m
+++ objective-c_practice/ex_NSArray_sortedArrayUsingFunction/ex_NSArray_sortedArrayUsingFunction/ViewController.m
@@ -8,6 +8,19 @@
 
 #import "ViewController.h"
 
+//比較関係の定義
+NSInteger intSort(id num1, id num2, void *context)
+{
+    int v1 = [num1 intValue];
+    int v2 = [num2 intValue];
+    if(v1 < v2)
+        return NSOrderedAscending;
+    else if(v1 > v2)
+        return NSOrderedDescending;
+    else
+        return NSOrderedSame;
+}
+
 @interface ViewController ()
 
 @end
@@ -18,6 +31,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSMutableArray *vlist = [NSMutableArray arrayWithCapacity:10];
+    NSInteger v;
+    for(int i=0; i<10; i++){
+        v = arc4random() % 30;
+        [vlist addObject:[NSNumber numberWithInteger:v]];
+    }
+    NSLog(@"vlist %@", vlist);  //vlistの確認
+    //ソートした配列を作る
+    NSArray *sortedvlist = [vlist sortedArrayUsingFunction:intSort context:nil];
+    NSLog(@"sortedvlist %@", sortedvlist);    //ソート後のsortedvlistの確認
 }
 
 - (void)didReceiveMemoryWarning
