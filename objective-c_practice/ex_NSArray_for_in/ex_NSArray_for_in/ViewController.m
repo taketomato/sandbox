3128318755e12d559c74ee8a9ccc48401e444eef
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-03 16:07:44 +0900

    Initial Commit

--- objective-c_practice/ex_NSArray_for_in/ex_NSArray_for_in/ViewController.m
+++ objective-c_practice/ex_NSArray_for_in/ex_NSArray_for_in/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  ex_substringFromIndex
+//  ex_NSArray_for_in
 //
 //  Created by tchiba on 2014/04/03.
 //  Copyright (c) 2014年 taketomato. All rights reserved.
@@ -18,11 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    NSString *address1 = @"神奈川県藤沢市鵠沼1-2-3";
-    NSLog(@"FromIndex %@", [address1 substringFromIndex:7]);
-    NSLog(@"toIndex %@", [address1 substringToIndex:4]);
-    NSString
 }
 
 - (void)didReceiveMemoryWarning
