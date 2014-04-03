b48cffbd4a1327e6fdfb6ed5572450fe984526a4
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-03 16:30:53 +0900

    Initial Commit

---
+++ objective-c_practice/ex_NSArray_sortedArrayUsingFunction/ex_NSArray_sortedArrayUsingFunctionTests/ex_NSArray_sortedArrayUsingFunctionTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_NSArray_sortedArrayUsingFunctionTests.m
+//  ex_NSArray_sortedArrayUsingFunctionTests
+//
+//  Created by tchiba on 2014/04/03.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_NSArray_sortedArrayUsingFunctionTests : XCTestCase
+
+@end
+
+@implementation ex_NSArray_sortedArrayUsingFunctionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
