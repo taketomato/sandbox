6497e389380098b96adfb1e4ec76732785bb2726
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-03 16:15:59 +0900

    [chore] 配列と高速列挙例

--- objective-c_practice/ex_NSArray_for_in/ex_NSArray_for_in/ViewController.m
+++ objective-c_practice/ex_NSArray_for_in/ex_NSArray_for_in/ViewController.m
@@ -18,6 +18,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *intlist = @[@16, @5, @25, @54 ];
+    NSInteger sum = 0;
+    //値をnumに取り出す
+    for( NSString *num in intlist){
+        //数値オブジェクトをintValueで計算できる数値に変換する
+        //NSString *numの中にintlistの中身を順番に入れる
+        sum += [num intValue];
+    }
+    NSLog(@"合計 %d" , sum );
 }
 
 - (void)didReceiveMemoryWarning
