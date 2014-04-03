129854f7a989cb64a0d1b1eb32590f5849bc0859
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-03 15:31:33 +0900

    Initial Commit

---
+++ objective-c_practice/ex_substringFromIndex/ex_substringFromIndex/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ex_substringFromIndex
+//
+//  Created by tchiba on 2014/04/03.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
