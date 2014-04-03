13c8f8a4a514d7888cc09e1544d0d9d067feb06c
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-03 15:34:29 +0900

    [chore] 文字切り出しの練習

--- objective-c_practice/ex_substringFromIndex/ex_substringFromIndex/ViewController.m
+++ objective-c_practice/ex_substringFromIndex/ex_substringFromIndex/ViewController.m
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *address1 = @"神奈川県藤沢市鵠沼1-2-3";
+    NSLog(@"FromIndex %@", [address1 substringFromIndex:7]);
+    NSLog(@"toIndex %@", [address1 substringToIndex:4]);
 }
 
 - (void)didReceiveMemoryWarning
