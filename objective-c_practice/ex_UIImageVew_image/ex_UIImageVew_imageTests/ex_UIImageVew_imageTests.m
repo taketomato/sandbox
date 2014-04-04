521ef83e05c89dffec766176ef851ca4a1f7c818
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-04 16:26:06 +0900

    Initial Commit

---
+++ objective-c_practice/ex_UIImageVew_image/ex_UIImageVew_imageTests/ex_UIImageVew_imageTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_UIImageVew_imageTests.m
+//  ex_UIImageVew_imageTests
+//
+//  Created by tchiba on 2014/04/04.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_UIImageVew_imageTests : XCTestCase
+
+@end
+
+@implementation ex_UIImageVew_imageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
--- objective-c_practice/ex_addSubView_Image/ex_addSubView_Image/ViewController.m
+++ objective-c_practice/ex_addSubView_Image/ex_addSubView_Image/ViewController.m
@@ -18,6 +18,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage * imageData = [UIImage imageNamed:@"origin13330787586763.jpg"];
+    
+    CGRect rect = CGRectMake(0, 0, 100, 100);
+    UIImageView * imageView = [[UIImageView alloc] initWithFrame:rect];
+    imageView.backgroundColor = [UIColor redColor];
+    
+    imageView.image = imageData;
+    imageView.contentMode = UIViewContentModeTopLeft;
+    imageView.ClipsToBounds = YES;
+    
+    imageView.center = self.view.center;
+    
+    [self.view addSubview:imageView];
 }
 
 - (void)didReceiveMemoryWarning
