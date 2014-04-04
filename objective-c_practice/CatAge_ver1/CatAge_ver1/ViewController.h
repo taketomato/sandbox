79c2bfdc772fa22ee99fa296e7df04ca8c096acc
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-04 11:00:17 +0900

    Initial Commit

--- objective-c_practice/CatAge_ver1/CatAge_ver1/ViewController.h
+++ objective-c_practice/CatAge_ver1/CatAge_ver1/ViewController.h
@@ -1,6 +1,6 @@
 //
 //  ViewController.h
-//  ex_OutletCollection
+//  CatAge_ver1
 //
 //  Created by tchiba on 2014/04/04.
 //  Copyright (c) 2014年 taketomato. All rights reserved.
@@ -9,6 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-@property (strong, nonatomic) IBOutletCollection(UITextField) NSArray *colorNames;
 
 @end
--- objective-c_practice/CatAge_ver1/CatAge_ver1/ViewController.m
+++ objective-c_practice/CatAge_ver1/CatAge_ver1/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  ex_OutletCollection
+//  CatAge_ver1
 //
 //  Created by tchiba on 2014/04/04.
 //  Copyright (c) 2014年 taketomato. All rights reserved.
@@ -18,9 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    for(UITextField *fld in _colorNames){
-        fld.placeholder = @"好きな色は?";
-    }
 }
 
 - (void)didReceiveMemoryWarning
