1e8602b21119e3aa7dedbc3575c9172ee8c03cee
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-07 13:16:34 +0900

    Initial Commit

---
+++ objective-c_practice/ex_UIButton_image/ex_UIButton_image/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ex_UIButton_image
+//
+//  Created by tchiba on 2014/04/07.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ objective-c_practice/ex_UIButton_image/ex_UIButton_imageTests/ex_UIButton_imageTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_UIButton_imageTests.m
+//  ex_UIButton_imageTests
+//
+//  Created by tchiba on 2014/04/07.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_UIButton_imageTests : XCTestCase
+
+@end
+
+@implementation ex_UIButton_imageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
