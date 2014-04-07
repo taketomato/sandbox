40d5b4fa34a85bbfc463dd2462e2fafdcf46d32a
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-07 13:07:19 +0900

    Initial Commit

--- objective-c_practice/ex_UIButton_addTarget/ex_UIButton_addTarget/ViewController.m
+++ objective-c_practice/ex_UIButton_addTarget/ex_UIButton_addTarget/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+//ボタンで実行するhelloメソッドの宣言
+- (void)hello:(UIButton *)sender;
 @end
 
 @implementation ViewController
@@ -18,12 +19,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //ボタンを作る
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    //タイトルとフォント
+    [button setTitle:@"Hello" forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    //文字色と背景色
+    button.tintColor = [UIColor whiteColor];
+    button.backgroundColor = [UIColor redColor];
+    //表示位置と縦横サイズ
+    button.frame = CGRectMake(50, 50, 100, 40);
+    //ボタンにアクションを追加する
+    [button addTarget:self action:@selector(hello:) forControlEvents:UIControlEventTouchUpInside];
+    //ボタンをビューに追加する
+    [self.view addSubview:button];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
+
+//ボタンのタッチで実行するメソッド
+- (void)hello:(UIButton *)sender
+{
+    NSLog(@"ハロー");
+}
+
 
 @end
---
+++ objective-c_practice/ex_UIButton_enabled/ex_UIButton_enabled/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ex_UIButton_enabled
+//
+//  Created by tchiba on 2014/04/07.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
