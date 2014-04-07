ae61814e64d2aaf594633bdeb02912b64faaa022
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-07 10:14:53 +0900

    Initial Commit

--- objective-c_practice/ex_bringSubViewToFront/ex_bringSubViewToFront/ViewController.m
+++ objective-c_practice/ex_bringSubViewToFront/ex_bringSubViewToFront/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  ex_inseretSubview-2
+//  ex_bringSubViewToFront
 //
 //  Created by tchiba on 2014/04/07.
 //  Copyright (c) 2014年 taketomato. All rights reserved.
@@ -9,8 +9,6 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *dai;
-- (IBAction)valueChanged:(UIStepper *)sender;
 
 @end
 
@@ -28,7 +26,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)valueChanged:(UIStepper *)sender {
-    [self.view insertSubview:_dai atIndex:sender.value];
-}
 @end
