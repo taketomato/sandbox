60eab0ca35dc1b50baec46086b8ec0e2e7c0cb1e
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-07 16:30:28 +0900

    Initial Commit

--- objective-c_practice/ex_UIPanGR-drag_affine/ex_UIPanGR-drag_affine/ViewController.m
+++ objective-c_practice/ex_UIPanGR-drag_affine/ex_UIPanGR-drag_affine/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    //移動ベクトルを保存
+    CGPoint prevTranslation;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *tombo;
+- (IBAction)dragging:(UIPanGestureRecognizer *)sender;
 
 @end
 
@@ -26,4 +32,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)dragging:(UIPanGestureRecognizer *)sender {
+    //ドラッグ移動したベクトル
+    CGPoint translation = [sender translationInView:self.view];
+    if(sender.state == UIGestureRecognizerStateBegan){
+        //前回の続きから開始する
+        [sender setTranslation:prevTranslation inView:self.view];
+    }
+    else if(sender.state == UIGestureRecognizerStateChanged){
+        //ドラッグにあわせて移動させる
+        _tombo.transform = CGAffineTransformMakeTranslation(translation.x, translation.y);
+    }
+    else if(sender.state == UIGestureRecognizerStateEnded){
+        //ドラッグ操作終了時の移動ベクトルを保存する
+        prevTranslation = translation;
+    }
+}
 @end
---
+++ objective-c_practice/ex_UIRotationGR/ex_UIRotationGRTests/ex_UIRotationGRTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_UIRotationGRTests.m
+//  ex_UIRotationGRTests
+//
+//  Created by tchiba on 2014/04/07.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_UIRotationGRTests : XCTestCase
+
+@end
+
+@implementation ex_UIRotationGRTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
