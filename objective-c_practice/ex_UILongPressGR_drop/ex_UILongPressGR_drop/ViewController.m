9c1f0bf70df28421d3a5e8a63d2a7b317937d272
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-08 09:22:36 +0900

    Initial Commit

---
+++ objective-c_practice/ex_UILongPressGR_drop/ex_UILongPressGR_drop/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ex_UILongPressGR_drop
+//
+//  Created by tchiba on 2014/04/08.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- objective-c_practice/ex_UIPinchGR/ex_UIPinchGR/ViewController.m
+++ objective-c_practice/ex_UIPinchGR/ex_UIPinchGR/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    //スケールを保存するインスタンス変数
+    CGFloat prevScale;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *flower;
+- (IBAction)pinchFlower:(UIPinchGestureRecognizer *)sender;
 
 @end
 
@@ -18,6 +24,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //初期化
+    prevScale = 1.0;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +34,18 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)pinchFlower:(UIPinchGestureRecognizer *)sender {
+    if(sender.state == UIGestureRecognizerStateBegan){
+        //前回のピンチ操作のスケールにセットする
+        sender.scale = prevScale;
+    }
+    else if(sender.state == UIGestureRecognizerStateChanged) {
+        //スケールにあわせて拡大縮小する
+        _flower.transform = CGAffineTransformMakeScale(sender.scale, sender.scale);
+    }
+    else if(sender.state == UIGestureRecognizerStateEnded) {
+        //ピンチ操作終了時のスケールを保存する
+        prevScale = sender.scale;
+    }
+}
 @end
