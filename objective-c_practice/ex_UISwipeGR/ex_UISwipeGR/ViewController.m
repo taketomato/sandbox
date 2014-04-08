6439093451a27044466fddf33bd58be15bc5c5bd
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-08 15:18:52 +0900

    Initial Commit

--- objective-c_practice/ex_UISwipeGR/ex_UISwipeGR/ViewController.m
+++ objective-c_practice/ex_UISwipeGR/ex_UISwipeGR/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *frontView;
+- (IBAction)swipeView:(UISwipeGestureRecognizer *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *coverButton;
+- (IBAction)closeFrontView:(UIButton *)sender;
 
 @end
 
@@ -18,6 +22,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //カバーボタンを隠す
+    _coverButton.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +32,35 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)swipeView:(UISwipeGestureRecognizer *)sender {
+    CGPoint location = _frontView.center;
+    CGFloat center_x = self.view.center.x;
+    
+    //スワイプした方向で分岐する
+    if(sender.direction == UISwipeGestureRecognizerDirectionRight){
+        //右にスワイプしたので右に開ける
+        location.x = center_x + 120;
+        _coverButton.hidden = NO;
+    }
+    else if(sender.direction == UISwipeGestureRecognizerDirectionLeft){
+        //左にスワイプしたので左に閉める(元の位置へ)
+        location.x = center_x;
+        _coverButton.hidden = YES;
+    }
+    
+    //_frontViewを左右に開け閉めするアニメーション
+    [UIView animateWithDuration:0.5
+                     animations:^{
+                         _frontView.center = location;
+                     }];
+}
+- (IBAction)closeFrontView:(UIButton *)sender {
+    //カバーボタンを隠す
+    _coverButton.hidden = YES;
+    //_coverViewを閉めるアニメーション
+    [UIView animateWithDuration:0.5
+                     animations:^{
+                         _frontView.center = CGPointMake(160, 284);
+                     }];
+}
 @end
---
+++ objective-c_practice/ex_addGestureRecognizer-image/ex_addGestureRecognizer-imageTests/ex_addGestureRecognizer_imageTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_addGestureRecognizer_imageTests.m
+//  ex_addGestureRecognizer-imageTests
+//
+//  Created by tchiba on 2014/04/08.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_addGestureRecognizer_imageTests : XCTestCase
+
+@end
+
+@implementation ex_addGestureRecognizer_imageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
