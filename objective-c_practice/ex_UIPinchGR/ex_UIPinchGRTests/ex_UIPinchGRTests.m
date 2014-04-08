88841493f398d2064b5df1d2d8d9261b48c20cc7
tchiba <Taketo.Chiba@jrits.ricoh.co.jp>
2014-04-08 09:14:01 +0900

    Initial Commit

---
+++ objective-c_practice/ex_UIPinchGR/ex_UIPinchGRTests/ex_UIPinchGRTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_UIPinchGRTests.m
+//  ex_UIPinchGRTests
+//
+//  Created by tchiba on 2014/04/08.
+//  Copyright (c) 2014年 taketomato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_UIPinchGRTests : XCTestCase
+
+@end
+
+@implementation ex_UIPinchGRTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
--- objective-c_practice/ex_UIRotationGR/ex_UIRotationGR/ViewController.m
+++ objective-c_practice/ex_UIRotationGR/ex_UIRotationGR/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    //回転角度を保存するインスタンス変数
+    CGFloat prevRotation;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *tombo;
+- (IBAction)rotateTombo:(UIRotationGestureRecognizer *)sender;
 
 @end
 
@@ -26,4 +32,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)rotateTombo:(UIRotationGestureRecognizer *)sender {
+    if(sender.state == UIGestureRecognizerStateBegan){
+        //前回の続きから回転する
+        sender.rotation = prevRotation;
+    }
+    else if(sender.state == UIGestureRecognizerStateChanged){
+        //連続して回している間、トンボの角度を合わせる
+        _tombo.transform = CGAffineTransformMakeRotation(sender.rotation);
+    }else if(sender.state == UIGestureRecognizerStateEnded){
+        //操作終了時の回転角度を記録する
+        prevRotation = sender.rotation;
+    }
+}
 @end
